179a784cfaea4a6d3da874f7dc278987d0d067e9
o-some <eleftherios@o-some.de>
2026-08-19 23:22:26 +0200

    chore: configure Astro for GitHub Pages base path

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://o-some.github.io',
+  base: '/letter-bay',
+  output: 'static',
+  trailingSlash: 'always',
+});
